8a4ca581962cf15be8fb6d62f70a7929f474e1f8
okhremenko59 <okhremenko@bk.ru>
2022-09-21 14:40:12 +0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,28 @@ public class Main {
         personList.add(new Person("Nadya", "Vaselkova Nekifirova Nebrezko", 12));
         personList.add(new Person("Olya", "Vaselkova Nekifirova Nebrezko Nekrasova", 24));
 
+        Comparator<Person> comp = (o1, o2) -> {
+            Set<String> cnt1 = new HashSet<>((List.of(o1.getSurname().split("\\P{IsAlphabetic}+"))));
+            Set<String> cnt2 = new HashSet<>((List.of(o2.getSurname().split("\\P{IsAlphabetic}+"))));
+            if (cnt1.size() > cnt2.size()) {
+                return 1;
+            } else if (cnt1.size() < cnt2.size()) {
+                return -1;
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
+
         System.out.println(personList);
-        personList.sort(new PersonComparator());
+        personList.sort(comp);
         Collections.reverse(personList);
         System.out.println(personList);
 
         Predicate<Person> predicate = Main::test;
         personList.removeIf(predicate);
+
+        System.out.println(personList);
     }
-
-
 
     private static boolean test(Person personList) {
         return personList.getAge() < 18;
